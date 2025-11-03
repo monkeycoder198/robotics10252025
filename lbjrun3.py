3762d1b0313225ace034377c2aa267d8d22490a1
monkeycoder198 <109483504+monkeycoder198@users.noreply.github.com>
2025-11-03 07:55:37 -0800

    Update lbjrun3.py

--- lbjrun3.py
+++ lbjrun3.py
@@ -4,32 +4,29 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
+
 hub = PrimeHub()
-db=DriveBase(Motor(Port.F, Direction.COUNTERCLOCKWISE), Motor(Port.B, Direction.CLOCKWISE), 56, 84)
+lbj=DriveBase(Motor(Port.F, Direction.COUNTERCLOCKWISE), Motor(Port.B), 56, 84)
+lbj.use_gyro(True)
 
-#Mission 6 - Run 3 - Dest. 1 
-'''db.settings(970,970)
-db.straight(745)
-db.turn(-6)
-db.settings(50,50)
-db.straight(15)
-db.settings(970,970)
-db.straight(-215)'''
 
-db.use_gyro(True)
-db.settings(500, 425)
-db.straight(720)
-db.turn(-15)
-db.straight(30)
-db.straight(-25)
-db.turn(15)
-db.straight(-200)
+def run3():
+  lbj.settings(700,350)
+  lbj.straight(575)
 
-db.turn(-16.5)
-db.straight(263)
-db.settings(700, None, 400, 600)
-db.turn(-75)
 
-db.straight(250)
-db.turn(35)
-db.straight(-1000)
+  lbj.settings(200,100)
+  lbj.straight(25)
+  lbj.settings(970,970)
+   `
+  #lbj.turn(-5)
+  lbj.straight(-200)
+  lbj.straight(-200)
+
+
+  lbj.curve(550,-30)
+  lbj.curve(550,30)
+  lbj.straight(-550)
+
+
+run3()
