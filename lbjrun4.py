2cf6dacf3f6790548c8c603a4f6ff6f348f21d9e
monkeycoder198 <109483504+monkeycoder198@users.noreply.github.com>
2025-11-03 07:56:12 -0800

    Update lbjrun4.py

--- lbjrun4.py
+++ lbjrun4.py
@@ -4,23 +4,44 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
+
 hub = PrimeHub()
+db=DriveBase(Motor(Port.F, Direction.COUNTERCLOCKWISE), Motor(Port.B, Direction.CLOCKWISE), 56, 84)
 
-lbj=DriveBase(Motor(Port.F, Direction.COUNTERCLOCKWISE), Motor(Port.B), 56, 84)
-lbj.use_gyro(True)
 
-lbj.settings(700,700,970,970)
-lbj.straight(1045)
-lbj.turn(80)
+#Mission 6 - Run 3 - Dest. 1
+'''db.settings(970,970)
+db.straight(745)
+db.turn(-6)
+db.settings(50,50)
+db.straight(15)
+db.settings(970,970)
+db.straight(-215)'''
 
-lbj.straight(135)
-#Repeater Loop - Do the mission with the rope (Decent amount of points)
-#We can only swivel it in one direction :(
 
-for _ in range(1,16):
-   lbj.turn(60)
-   lbj.turn(-60)
-   lbj.straight(-2)
-lbj.straight(-120)
-lbj.turn(-70)
-lbj.straight(700)
+db.use_gyro(True)
+db.settings(500, 425)
+db.straight(720)
+db.turn(-15)
+db.straight(30)
+db.straight(-25)
+db.turn(15)
+db.straight(-200)
+
+
+db.turn(-16.5)
+db.straight(263)
+db.settings(700, 400, 600)
+db.turn(-75)
+db.straight(350)
+db.turn(-90)
+
+
+"""
+db.straight(243)
+db.turn(40)
+db.settings(500,300)
+#db.arc(100000, -1100)
+db.straight(-1100)
+db.straight(300)
+"""
